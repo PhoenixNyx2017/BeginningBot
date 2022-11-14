bfdb3c087242f69f1a927ccbea34181ba48653f0
Michelle Guo <runbo.guo.17@gmail.com>
2022-11-14 17:31:26 -0500

    Second comment for fun

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -32,6 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
+    // Hi I'm comment 2
     m_robotContainer = new RobotContainer();
     // Hi I'm a comment
   }
